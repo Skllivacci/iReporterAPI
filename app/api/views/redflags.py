8badec3cfdcee1cdb0ee31755ff9366cbda8b3c4
Skllivacci <jonahkarash@gmail.com>
2018-11-29 18:39:08 +0300

    Add get all redflags method [finishes #162308190]

---
+++ app/api/views/redflags.py
@@ -0,0 +1,48 @@
+from flask_restful import Resource
+from flask import jsonify, make_response, request, abort
+
+import datetime
+
+incidents = [] 
+
+class RedFlags(Resource):
+    """docstring for RedFlags"""
+    
+    def __init__(self):
+        self.db = incidents
+        self.id = len(incidents) + 1
+       
+    
+    def post(self):
+        
+        data = {
+            'id' : self.id,
+            'createdOn' : datetime.datetime.utcnow(),
+            'createdBy' : request.json['createdBy'],
+            'type' : 'red-flags',
+            'location' : request.json['location'],
+            'status' : "Under Investigation",
+            'images' : request.json['images'],
+            'videos' : request.json['videos'],
+            'title' : request.json['title'],
+            'comment' : request.json['comment']
+        }
+        self.db.append(data)
+        
+        success_message = {
+            'id' : self.id,
+            'message' : 'Thank You for Creating a Red-Flag'
+        }
+
+        return make_response(jsonify({
+            "status" : 201,
+            "data" : success_message
+        }), 201)
+
+         def get(self):
+
+        return make_response(jsonify({
+            "status" : 200,
+            "data" : self.db
+        }), 200) 
+       
